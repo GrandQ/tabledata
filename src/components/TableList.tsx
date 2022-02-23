1d77c347ee5f0c274dbda201599abf91eaa8017f
Radek <granddw4@gmail.com>
2022-02-23 20:36:49 +0100

    added searchbar

--- src/components/TableList.tsx
+++ src/components/TableList.tsx
@@ -1,20 +1,32 @@
+import React, { useState } from "react";
 import { items } from "../interfaces/itemType";
 
 const TableList = ({ items }: {items: items[]}) => {
+    const [search, setSearch] = useState("");
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value)
+    }
+    const handleSearchFilter = ((val: items) => {
+        if (search === "") return val
+        else if (val.description.toLowerCase().includes(search.toLowerCase())) return val
+    })
     return(
-        <div className="table-container">
-                {Object.values(items).map((item: items) => (
-                    <div key={item.work_order_id} className="table-wrapper">
-                        <ul key={2}>
-                            <li key={3} className="flexRow">{item.work_order_id}</li>
-                            <li key={4} className="flexRow desc">{item.description}</li>
-                            <li key={5} className="flexRow">{item.received_date}</li>
-                            <li key={6} className="flexRow">{item.assigned_to.person_name}</li>
-                            <li key={7} className="flexRow">{item.status}</li>
-                            <li key={8} className="flexRow">{item.priority}</li>
-                        </ul>
-                    </div>
-                ))}
+        <div className="main">
+            <input type="text" placeholder="Search..." onChange={handleSearch} />
+            <div className="table-container">
+                    {Object.values(items).filter(handleSearchFilter).map((item: items) => (
+                        <div key={item.work_order_id} className="table-wrapper">
+                            <ul key={2}>
+                                <li key={3} className="flexRow">{item.work_order_id}</li>
+                                <li key={4} className="flexRow desc">{item.description}</li>
+                                <li key={5} className="flexRow">{item.received_date}</li>
+                                <li key={6} className="flexRow">{item.assigned_to.person_name}</li>
+                                <li key={7} className="flexRow">{item.status}</li>
+                                <li key={8} className="flexRow">{item.priority}</li>
+                            </ul>
+                        </div>
+                    ))}
+            </div>
         </div>
     )
 };
--- src/interfaces/itemType.tsx
+++ src/interfaces/itemType.tsx
@@ -9,29 +9,3 @@ export interface items {
     status: string;
     priority: string;
 }
-
-/* 
-export interface items {
-    exec_time: number, 
-    response: {
-        current_page: number;
-        from: number;
-        last_page: number;
-        per_page: number;
-        to: number;
-        total: number;
-        data: {
-            work_order_id: number;
-            description: string;
-            received_date: string; 
-            assigned_to: {
-                person_name: string; 
-                status: string;
-            }
-            status: string;
-            priority: string;
-        }
-    }
-}
-
-*/
